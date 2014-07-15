9384db42652d119b63d0af74b08fd7770266edee
zgmartin <zacharygilmartin@gmail.com>
2014-07-15 01:59:34 -0400

    hello word file

---
+++ HelloGit.java
@@ -0,0 +1,13 @@
+/*
+author: Zachary Gilmartin
+date: Mon, July 14, 2014
+about: a program to say hello to the git world
+*/
+
+public class HelloGit
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello GitHub!");
+  }
+}

2c96020f135edd7c1264cf22da32fdc72a74a2ee
Zachary Gilmaritin <zgmartin@users.noreply.github.com>
2014-09-24 12:07:47 -0400

    Update and rename HelloGit.java to hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+class Wrold:
+  """This is the World. It knows the answer to life."""
+  
+  def greeting(self):
+    print 'you are flying with python!'
+    
+  def the_answer(self):
+    return 43
